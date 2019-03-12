676ca4b75e362b54e0602013d6ccb4464b7c7f78
Candice Davidson <cndragn@gmail.com>
2019-03-11 20:06:11 -0700

    reorganize index

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+const Header = () => (
+	<header
+		style={{
+			background: `rebeccapurple`,
+			marginBottom: `1.45rem`
+		}}
+	>
+		<div
+			style={{
+				margin: `0 auto`,
+				maxWidth: 960,
+				padding: `1.45rem 1.0875rem`
+			}}
+		>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{
+						color: `white`,
+						textDecoration: `none`
+					}}
+				/>
+			</h1>
+		</div>
+	</header>
+);
+
+export default Header;
