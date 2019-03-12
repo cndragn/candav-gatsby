ea1f43115b34ae41e17b6d99593d3a20f3d39386
Candice Davidson <cndragn@gmail.com>
2019-03-11 21:38:11 -0700

    add in style and portfolio css files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/style.css';
+import '../css/portfolio.css';
 
 import SEO from '../components/seo';
 import Header from '../components/Header';
