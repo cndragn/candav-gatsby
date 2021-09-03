f28cb88c6cbabb2eef75e6c64b1327444781192e
Candice Davidson <cndragn@gmail.com>
2021-09-03 16:27:01 -0700

    add statcounter to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,39 +5,68 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
 
-import Header from './PageHeader';
-import Footer from './Footer';
-import './layout.css';
+import Header from "./PageHeader"
+import Footer from "./Footer"
+import "./layout.css"
 
 const Layout = ({ children }) => (
-	<StaticQuery
-		query={graphql`
-			query SiteTitleQuery {
-				site {
-					siteMetadata {
-						title
-					}
-				}
-			}
-		`}
-		render={(data) => (
-			<div>
-				<Header siteTitle={data.site.siteMetadata.title} />
-				<div>
-					<main className="page">{children}</main>
-					<Footer />
-				</div>
-			</div>
-		)}
-	/>
-);
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div>
+          <main className="page">{children}</main>
+          <Footer />
+        </div>
+        <div className="statcounter">
+          <script type="text/javascript">
+            var sc_project=3844398; var sc_invisible=1; var
+            sc_security="34f6984a";
+          </script>
+          ,
+          <script
+            type="text/javascript"
+            src="https://www.statcounter.com/counter/counter.js"
+            async
+          ></script>
+          ,
+          <noscript>
+            <div className="statcounter">
+              <a
+                title="Web Analytics"
+                href="https://statcounter.com/"
+                target="_blank"
+              >
+                <img
+                  className="statcounter"
+                  src="https://c.statcounter.com/3844398/0/34f6984a/1/"
+                  alt="Web Analytics"
+                  referrerPolicy="no-referrer-when-downgrade"
+                />
+              </a>
+            </div>
+          </noscript>
+        </div>
+      </div>
+    )}
+  />
+)
 
 Layout.propTypes = {
-	children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
+export default Layout
