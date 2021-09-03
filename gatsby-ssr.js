1c6fc6cd22418df07bc1be58b0c45f2345f2bdd8
Candice Davidson <cndragn@gmail.com>
2021-09-03 16:33:01 -0700

    add stat to everything

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -12,26 +12,33 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
     <script type="text/javascript">
       var sc_project=3844398; var sc_invisible=1; var sc_security="34f6984a";
     </script>,
-    <script
-      type="text/javascript"
-      src="https://www.statcounter.com/counter/counter.js"
-      async
-    ></script>,
-    <noscript>
-      <div class="statcounter">
-        <a
-          title="Web Analytics"
-          href="https://statcounter.com/"
-          target="_blank"
-        >
-          <img
-            class="statcounter"
-            src="https://c.statcounter.com/3844398/0/34f6984a/1/"
-            alt="Web Analytics"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
-        </a>
-      </div>
-    </noscript>,
+    <div className="statcounterSsr">
+      <script type="text/javascript">
+        var sc_project=3844398; var sc_invisible=1; var sc_security="34f6984a";
+      </script>
+
+      <script
+        type="text/javascript"
+        src="https://www.statcounter.com/counter/counter.js"
+        async
+      ></script>
+
+      <noscript>
+        <div className="statcounter">
+          <a
+            title="Web Analytics"
+            href="https://statcounter.com/"
+            target="_blank"
+          >
+            <img
+              className="statcounter"
+              src="https://c.statcounter.com/3844398/0/34f6984a/1/"
+              alt="Web Analytics"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </a>
+        </div>
+      </noscript>
+    </div>,
   ])
 }
