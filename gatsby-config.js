bed0264157eac0baea375a19d38c8d74fd12589c
Candice Davidson <cndragn@gmail.com>
2021-09-03 17:32:15 -0700

    insrtall google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,92 @@
 module.exports = {
-	siteMetadata: {
-		title: `Candice Davidson`,
-		description: `Software Engineer`,
-		author: `Candice Davidson`,
-		siteUrl: `http://www.candicedavidson.com`
-	},
-	plugins: [
-		`gatsby-plugin-catch-links`,
-		`gatsby-plugin-react-helmet`,
-		`gatsby-transformer-remark`,
-		{
-			resolve: `gatsby-remark-prismjs`,
-			options: {
-				classPrefix: 'language-',
-				inlineCodeMarker: null,
-				aliases: {},
-				showLineNumbers: false,
-				noInlineHighlight: false
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/images`
-			}
-		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: `src`,
-				path: `${__dirname}/src`
-			}
-		},
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
-				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
-				display: `minimal-ui`,
-				icon: `src/images/davidson-icon.png` // This path is relative to the root of the site.
-			}
-		},
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		'gatsby-plugin-offline',
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				path: `${__dirname}/src/pages`,
-				name: 'pages'
-			}
-		}
-	]
-};
+  siteMetadata: {
+    title: `Candice Davidson`,
+    description: `Software Engineer`,
+    author: `Candice Davidson`,
+    siteUrl: `http://www.candicedavidson.com`,
+  },
+  plugins: [
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-remark-prismjs`,
+      options: {
+        classPrefix: "language-",
+        inlineCodeMarker: null,
+        aliases: {},
+        showLineNumbers: false,
+        noInlineHighlight: false,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/davidson-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-5723962-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "",
+        // Enables Google Optimize Experiment ID
+        experimentId: "",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "auto",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
+  ],
+}
