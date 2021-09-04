a7416c0a707a5549f44002db281cc06703e18c8a
Candice Davidson <cndragn@gmail.com>
2021-09-04 12:05:01 -0700

    pare down google analytics code to necessities

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,26 +13,6 @@ module.exports = {
         trackingId: "UA-5723962-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // Enables Google Optimize Experiment ID
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "0",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "auto",
-        // defaults to false
-        enableWebVitalsTracking: true,
       },
     },
     `gatsby-plugin-catch-links`,
